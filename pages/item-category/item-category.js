a33905183d9ab89fb3e5fbef59525b1867b919fa
qinshengda <1158129400@qq.com>
2020-10-19 18:38:18 +0800

    美购分类页面调整

--- pages/item-category/item-category.js
+++ pages/item-category/item-category.js
@@ -5,46 +5,108 @@ Page({
    * 页面的初始数据
    */
   data: {
-    currentIndex:0,
-    list:[
-      {
-        title:'皮肤管理',
-        lower:[
-          {name:'光子嫩肤'},
-          {name:'皮秒'},
-          {name:'超皮秒'},
-          {name:'朝批'},
-          {name:'哈哈'},
+    currentIndex: 0,
+    list: [{
+        title: '皮肤管理',
+        lower: [{
+            name: '光子嫩肤'
+          },
+          {
+            name: '皮秒'
+          },
+          {
+            name: '超皮秒'
+          },
+          {
+            name: '朝批'
+          },
+          {
+            name: '哈哈'
+          },
         ]
       },
       {
-        title:'玻尿酸',
-        lower:[
-          {name:'光子嫩肤'},
-          {name:'皮秒'},
-          {name:'超皮秒'},
-          {name:'朝批'},
-          {name:'哈哈'},
+        title: '玻尿酸',
+        lower: [{
+            name: '光子嫩肤'
+          },
+          {
+            name: '皮秒'
+          },
+          {
+            name: '超皮秒'
+          },
+          {
+            name: '朝批'
+          },
+          {
+            name: '哈哈'
+          },
         ]
       },
       {
-        title:'其他',
-        lower:[
-          {name:'光子嫩肤'},
-          {name:'皮秒'},
-          {name:'超皮秒'},
-          {name:'朝批'},
-          {name:'哈哈'},
+        title: '其他',
+        lower: [{
+            name: '光子嫩肤'
+          },
+          {
+            name: '皮秒'
+          },
+          {
+            name: '超皮秒'
+          },
+          {
+            name: '朝批'
+          },
+          {
+            name: '哈哈'
+          },
+        ]
+      },
+      {
+        title: '其他',
+        lower: [{
+            name: '光子嫩肤'
+          },
+          {
+            name: '皮秒'
+          },
+          {
+            name: '超皮秒'
+          },
+          {
+            name: '朝批'
+          },
+          {
+            name: '哈哈'
+          },
+          {
+            name: '光子嫩肤'
+          },
+          {
+            name: '皮秒'
+          },
+          {
+            name: '超皮秒'
+          },
+          {
+            name: '朝批'
+          },
+          {
+            name: '哈哈'
+          }
         ]
       }
-    ]
+    ],
+    topList: [],
+    scrollTop:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getHieght()
   },
 
   /**
@@ -60,14 +122,45 @@ Page({
   onShow: function () {
 
   },
+  scroll: function (e) {
+    console.log('结果', e.detail.scrollTop)
+    for (let i = 0; i < this.data.topList.length; i++) {
+      if (e.detail.scrollTop <= this.data.topList[i]) {
+        this.setData({
+          currentIndex: i
+        })
+        break;
+      }
+    }
 
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
 
   },
+  //回到顶部
+  toTop: function (e) { // 一键回到顶部
+    const idx = e.currentTarget.dataset.idx
+    this.topScroll(idx)
+  },
+  topScroll: function (idx) {
+     this.setData({
+       scrollTop: this.data.topList[idx]-this.data.topList[0]
+     })
 
+  },
+  getHieght() {
+    var that = this
+    for (let i = 0; i < this.data.list.length; i++) {
+      var query = wx.createSelectorQuery();
+      //选择id
+      query.select('.itemH' + i).boundingClientRect(function (rect) {
+        that.data.topList.push(rect.top)
+      }).exec();
+    }
+  },
   /**
    * 生命周期函数--监听页面卸载
    */
